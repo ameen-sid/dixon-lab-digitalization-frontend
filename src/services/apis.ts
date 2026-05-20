af46abdbd547dbb9ef39fc6d0ef454002b8a0cad
Ameen Sid <ameensid7@outlook.com>
2026-05-20 15:37:33 +0530

    Added test type's api endpoints in the apis

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -21,3 +21,11 @@ export const userEndpoints = {
 	UPDATE_USER_API: (id: string | number) => BASE_URL + `/users/${id}`,
 	DELETE_USER_API: (id: string | number) => BASE_URL + `/users/${id}`,
 };
+
+// Test Type Endpoints
+export const testTypeEndpoints = {
+	GET_TEST_TYPES_API: BASE_URL + '/test-types',
+	CREATE_TEST_TYPE_API: BASE_URL + '/test-types',
+	UPDATE_TEST_TYPE_API: (id: string | number) => BASE_URL + `/test-types/${id}`,
+	DELETE_TEST_TYPE_API: (id: string | number) => BASE_URL + `/test-types/${id}`,
+};
